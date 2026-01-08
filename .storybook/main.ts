1bd2edd5232200ec5f9d4def1cd0f9995c37140a
Gregory Douglas <gdouglas@palantir.com>
2026-01-08 17:13:55 -0500

    Update button stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,27 +1,31 @@
+/* !
+ * (c) Copyright 2026 Palantir Technologies Inc. All rights reserved.
+ */
+
 import type { StorybookConfig } from "@storybook/react-vite";
 import { mergeConfig } from "vite";
 
-const config: StorybookConfig = {
-    stories: ["../packages/{core,datetime,labs,select,table}/src/**/*.stories.@(ts|tsx)"],
+const storybookConfig: StorybookConfig = {
     addons: ["@storybook/addon-links", "@storybook/addon-docs"],
+    core: {
+        disableTelemetry: true,
+    },
+    stories: ["../packages/{core,datetime,labs,select,table}/src/**/*.stories.@(ts|tsx)"],
     framework: {
         name: "@storybook/react-vite",
         options: {},
     },
-    core: {
-        disableTelemetry: true,
-    },
     async viteFinal(config) {
         return mergeConfig(config, {
-            resolve: {
-                dedupe: ["react", "react-dom"],
-            },
             optimizeDeps: {
                 // Force Vite to pre-bundle icons to avoid circular dependency with global Array
                 include: ["@blueprintjs/icons"],
+            },
+            resolve: {
+                dedupe: ["react", "react-dom"],
             },
         });
     },
 };
 
-export default config;
+export default storybookConfig;
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,20 +1,29 @@
+/* !
+ * (c) Copyright 2026 Palantir Technologies Inc. All rights reserved.
+ */
+
 import type { Preview } from "@storybook/react-vite";
+
+import { FocusStyleManager } from "../packages/core/src";
+import { Icons } from "../packages/icons/src/iconLoader";
+
+FocusStyleManager.onlyShowFocusOnTabs();
+
+Icons.setLoaderOptions({ loader: "all" });
+
+// optionally, load the icons up-front so that future usage does not trigger a network request
+await Icons.loadAll();
 
 // Import Blueprint compiled CSS
 import "../packages/core/lib/css/blueprint.css";
 import "../packages/datetime/lib/css/blueprint-datetime.css";
+
 // Note: Icons CSS is not imported globally to avoid dependency optimization issues.
 // Individual stories can import: import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 // if they need icon styling.
 
 const preview: Preview = {
     parameters: {
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/i,
-            },
-        },
         backgrounds: {
             default: "light",
             values: [
@@ -27,6 +36,12 @@ const preview: Preview = {
                     value: "#30404d",
                 },
             ],
+        },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
         },
     },
 };
